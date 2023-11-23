2fc0de7d67f9118488ca93280b0aa382286f6bda
adriannejulie <113201646+adriannejulie@users.noreply.github.com>
2023-11-23 11:22:09 -0700

    Add files via upload

---
+++ 480TermProjectData.sql
@@ -0,0 +1,223 @@
+DROP DATABASE IF EXISTS TP480;
+CREATE DATABASE TP480;
+USE TP480;
+
+DROP TABLE IF EXISTS EMPLOYEE;
+CREATE TABLE EMPLOYEE(
+    ID_NO               INT,
+    PRIMARY KEY(ID_NO)
+); 
+
+INSERT INTO EMPLOYEE(ID_NO)
+VALUE
+(1000), (1001), (1002), (1003), (1004), (1005), /* ID_NOs for Airline Agents*/
+(2000), (2001), (2002), (2003), (2004), (2005), /* ID_NOs for Pilots*/
+
+(3000), (3001), (3002), (3003), (3004), /* ID_NOs for Flight Attendants*/
+(3005), (3006), (3007), (3008), (3009), /* ID_NOs for Flight Attendants*/
+(3010), (3011), (3012), (3013), (3014), /* ID_NOs for Flight Attendants*/
+
+(3015), (3016), (3017), (3018), (3019), /* ID_NOs for Flight Attendants*/
+(3020), (3021), (3022), (3023), (3024), /* ID_NOs for Flight Attendants*/
+(3025), (3026), (3027), (3028), (3029), /* ID_NOs for Flight Attendants*/
+
+(3030), (3031), (3032), (3033), (3034), /* ID_NOs for Flight Attendants*/
+(3035), (3036), (3037), (3038), (3039), /* ID_NOs for Flight Attendants*/
+(3040), (3041), (3042), (3043), (3044), /* ID_NOs for Flight Attendants*/
+
+(4000), (4001), (4002), (4003), (4004), (4005); /* ID_NOs for System Admins*/
+
+DROP TABLE IF EXISTS AIRCRAFT;
+CREATE TABLE AIRCRAFT(
+    AIRCFRAFT_ID        INT,
+    PRIMARY KEY(AIRCFRAFT_ID)
+);
+
+INSERT INTO AIRCRAFT(AIRCFRAFT_ID) /* Assume all aircrafts have 80 seats*/
+VALUES
+(01), (02), (03), (04), (05), (06), (07), (08), (09), (10), (11), (12),
+(13), (14), (15), (16), (17), (18), (19), (20), (21), (22), (23), (24);
+
+DROP TABLE IF EXISTS AIRPORT;
+CREATE TABLE AIRPORT(
+    AIRPORT_CODE        VARCHAR(10),
+    AIRPORT_NAME        VARCHAR(100),
+    PRIMARY KEY(AIRPORT_CODE)
+);
+
+INSERT INTO AIRPORT(AIRPORT_CODE, AIRPORT_NAME)
+VALUES
+('CYYZ', 'Toronto Lester B. Pearson International Airport'),
+('CYVR', 'Vancouver International Airport'),
+('CYUL', 'Montreal / Pierre Elliott Trudeau International Airport'),
+('CYYC', 'Calgary International Airport'),
+('CYOW', 'Ottawa Macdonald-Cartier International Airport'),
+('CYTZ', 'Billy Bishop Toronto City Centre Airport'),
+('CYEG', 'Edmonton International Airport'),
+('CYWG', 'Winnipeg / James Armstrong Richardson International Airport');
+
+DROP TABLE IF EXISTS FLIGHT;
+CREATE TABLE FLIGHT(
+    FLIGHT_NUMBER       INT,
+    DESTINATION         VARCHAR(40),
+    ORIGIN              VARCHAR(40),
+    TIME_DEPARTURE      DATETIME,
+    AIRCRAFT_TYPE       INT,
+    PILOT1              INT,
+    PILOT2              INT,
+    FLIGHT_ATTENDANT1   INT,
+    FLIGHT_ATTENDANT2   INT,
+    FLIGHT_ATTENDANT3   INT,
+    FLIGHT_ATTENDANT4   INT,
+
+    PRIMARY KEY(FLIGHT_NUMBER),
+    FOREIGN KEY(AIRCRAFT_TYPE) REFERENCES AIRCRAFT(AIRCFRAFT_ID),
+    FOREIGN KEY(DESTINATION) REFERENCES AIRPORT(AIRPORT_CODE),
+    FOREIGN KEY(PILOT1) REFERENCES EMPLOYEE(ID_NO),
+    FOREIGN KEY(PILOT2) REFERENCES EMPLOYEE(ID_NO),
+    FOREIGN KEY(FLIGHT_ATTENDANT1) REFERENCES EMPLOYEE(ID_NO),
+    FOREIGN KEY(FLIGHT_ATTENDANT2) REFERENCES EMPLOYEE(ID_NO),
+    FOREIGN KEY(FLIGHT_ATTENDANT3) REFERENCES EMPLOYEE(ID_NO),
+    FOREIGN KEY(FLIGHT_ATTENDANT4) REFERENCES EMPLOYEE(ID_NO)
+);
+
+INSERT INTO FLIGHT(FLIGHT_NUMBER, DESTINATION, ORIGIN, TIME_DEPARTURE, AIRCRAFT_TYPE)
+VALUES
+(01, 'CYYZ','CYVR','2023-12-20 00:00:05', 01), /*Flights from Toronto to Vancouver*/
+(02, 'CYYZ','CYVR','2023-12-20 12:00:05', 02),
+(03, 'CYYZ','CYVR','2023-12-23 00:00:05', 03),
+(04, 'CYYZ','CYVR','2023-12-23 12:00:05', 04),
+
+(05, 'CYUL','CYYC','2023-12-25 00:00:05', 05), /*Flights from Montreal to Calgary*/
+(06, 'CYUL','CYYC','2023-12-25 12:00:05', 06),
+(07, 'CYUL','CYYC','2023-12-27 00:00:05', 07),
+(08, 'CYUL','CYYC','2023-12-27 12:00:05', 08),
+
+(09, 'CYOW','CYTZ','2023-12-29 00:00:05', 09), /*Flights from Ottowa to Toronto*/
+(10, 'CYOW','CYTZ','2023-12-29 12:00:05', 10),
+(11, 'CYOW','CYTZ','2023-12-31 00:00:05', 11),
+(12, 'CYOW','CYTZ','2023-12-31 12:00:05', 12),
+
+(13, 'CYEG','CYWG','2023-12-20 00:00:05', 13), /*Flights from Edmonton to Winnepeg*/
+(14, 'CYEG','CYWG','2023-12-20 12:00:05', 14),
+(15, 'CYEG','CYWG','2023-12-23 00:00:05', 15),
+(16, 'CYEG','CYWG','2023-12-23 12:00:05', 16),
+
+(17, 'CYYZ','CYYC','2023-12-25 00:00:05', 17), /*Flights from Toronto to Calgary*/
+(18, 'CYYZ','CYYC','2023-12-25 12:00:05', 18),
+(19, 'CYYZ','CYYC','2023-12-27 00:00:05', 19),
+(20, 'CYYZ','CYYC','2023-12-27 12:00:05', 20),
+
+(21, 'CYYC','CYVR','2023-12-29 00:00:05', 21), /*Flights from Calgary to Vancouver*/
+(22, 'CYYC','CYVR','2023-12-29 12:00:05', 22),
+(23, 'CYYC','CYVR','2023-12-31 00:00:05', 23),
+(24, 'CYYC','CYVR','2023-12-31 12:00:05', 24);
+
+
+DROP TABLE  IF EXISTS AIRLINE_AGENT;
+CREATE TABLE AIRLINE_AGENT(
+    AGENT_ID            INT,
+    PRIMARY KEY(AGENT_ID),
+    FOREIGN KEY(AGENT_ID) REFERENCES EMPLOYEE(ID_NO)
+);
+
+INSERT INTO AIRLINE_AGENT(AGENT_ID)
+VALUES
+(1000), (1001), (1002), (1003), (1004), (1005);
+
+
+DROP TABLE IF EXISTS CREW;
+CREATE TABLE CREW(
+    CREW_ID             INT,
+    PRIMARY KEY(CREW_ID),
+    FOREIGN KEY(CREW_ID) REFERENCES EMPLOYEE(ID_NO)
+);
+
+INSERT INTO CREW(CREW_ID)
+VALUES
+(2000), (2001), (2002), (2003), (2004), (2005), /* ID_NOs for Pilots*/
+(3000), (3001), (3002), (3003), (3004), (3005); /* ID_NOs for Flight Attendants*/
+
+
+DROP TABLE IF EXISTS ADMIN_STAFF;
+CREATE TABLE ADMIN_STAFF(
+    ADMIN_ID            INT,
+    PRIMARY KEY(ADMIN_ID),
+    FOREIGN KEY(ADMIN_ID) REFERENCES EMPLOYEE(ID_NO)
+);
+
+INSERT INTO ADMIN_STAFF(ADMIN_ID)
+VALUES
+(4000), (4001), (4002), (4003), (4004), (4005); 
+
+
+DROP TABLE IF EXISTS USERS;
+CREATE TABLE USERS(
+    ID_NO               INT,
+    NAME                VARCHAR(40),
+    BIRTH               DATE,
+    PHONE_NUMBER        VARCHAR(15),
+    CARD_NUMBER         VARCHAR(16),
+    EMAIL               VARCHAR(30),
+    FLIGHT_NUMBER       INT,
+    SEAT_TYPE           VARCHAR(15),
+    SEAT_NUMBER         INT,
+    MEMBERSHIP_ID       INT, /* MEMBERSHIP_ID == 0 if a non-member*/
+    INSURANCE_STATUS    INT, /* INSURANCE_STATUS == 1 if the customer has paid for insurance, otehrwise 0*/
+    PRIMARY KEY(ID_NO),
+    FOREIGN KEY(FLIGHT_NUMBER) REFERENCES  FLIGHT(FLIGHT_NUMBER)
+);
+
+INSERT INTO USERS(ID_NO, NAME, BIRTH, PHONE_NUMBER, CARD_NUMBER, EMAIL, FLIGHT_NUMBER, SEAT_TYPE, SEAT_NUMBER, MEMBERSHIP_ID, INSURANCE_STATUS)
+VALUES
+(0001, 'George Gower', '2000-12-13', '403-123-1234', '4111111111111111', 'GeorgeGower@example.com', 01, 'Comfort', 11, 0001, 1),
+(0002, 'Michiel Sittow', '2001-11-03', '467-321-2345', '5431111111111111', 'MichielSittow@example.com', 02, 'Ordinary', 26, 0000, 0),
+(0003, 'Lucas de Heere', '1999-09-11', '403-678-3875', '371111111111114', 'LucasdeHeere@example.com', 03, 'Business', 01, 0002, 1),
+(0004, 'Pablo Picasso', '1984-02-01', '403-098-0683', '36000000000008', 'PabloPicasso@example.com', 04, 'Comfort', 11, 0000, 0),
+(0005, 'Guillaume Coustou', '2003-06-22', '587-123-2356', '123455678901234', 'GuillaumeCoustou@example.com', 05, 'Ordinary', 26, 0003, 1),
+(0006, 'Bobby Brown', '1999-12-22', '403-753-2759', '6574839201928374', 'BobbyBrown@example.com', 06, 'Ordinary', 26, 0004, 1),
+(0007, 'Yuki Ishikawa', '1995-12-11', '467-192-1284', '1223344556677889', 'YukiIshikawa@example.com', 07, 'Comfort', 11, 0005, 0),
+(0008, 'Hershey Chips', '2003-05-22', '416-746-0298', '1019181716151413', 'HersheyChips@example.com', 08, 'Ordinary', 26, 0006, 0),
+(0009, 'Tim Hortons', '1965-04-21', '587-953-9923', '6564747384950628', 'TimHortons@example.com', 09, 'Business', 01, 0000, 1),
+(0010, 'Cal Garyian', '1974-06-23', '403-908-0085', '3436373829938465', 'CalGaryian@example.com', 10, 'Comfort', 11, 0007, 0),
+(0011, 'Ed Montonian', '1982-08-20', '587-002-9352', '2349765492738456', 'EdMontonian@example.com', 11, 'Business', 01, 0000, 1),
+(0012, 'Carey Beary', '2000-09-18', '403-034-8105', '0182736475839284', 'CareyBeary@example.com', 12, 'Ordinary', 26, 0000, 0),
+(0013, 'Nick Ey', '1965-10-16', '416-652-8234', '6193956244768703', 'NickEy@example.com', 13, 'Comfort', 11, 0008, 1),
+(0014, 'Stephano Montoya', '1945-10-15', '467-564-8723', '1253680986384572', 'StephanoMontoya@example.com', 14, 'Business', 01, 0000, 1),
+(0015, 'Sue Perstore', '1987-12-01', '467-079-7352', '9837483927463718', 'SuePerstore@example.com', 15, 'Business', 01, 0009, 0),
+(0016, 'John Wick', '1995-04-15', '416-938-8374', '4829175630986245', 'JohnWick@example.com', 16, 'Ordinary', 26,  0000, 1),
+(0017, 'Tess La', '1988-09-05', '403-124-0933', '7632891456723401', 'TessLa@example.com', 17, 'Ordinary', 26, 0010, 0),
+(0018, 'Mercy Des', '2000-08-18', '587-934-8245', '2156984710328765', 'MercyDes@example.com', 18, 'Comfort', 11, 0011, 0),
+(0019, 'Porcha Lux', '1992-12-10', '587-283-9084', '5974820136481093', 'PorchaLux@example.com', 19, 'Business', 01, 0012, 1),
+(0020, 'Bas Ketbal', '1985-03-30', '416-824-1872', '8310246570981537', 'BasKetbal@example.com', 20, 'Ordinary', 26, 0000, 1),
+(0021, 'Emily Thompson', '2002-07-12', '467-234-8753', '4567890123456789', 'EmilyThompson@example.com', 21, 'Ordinary', 26, 0000, 0),
+(0022, 'Olivia Chang', '1998-11-03', '403-935-9823', '1023745638905162', 'OliviaChang@example.com', 22, 'Ordinary', 26, 0000, 1),
+(0023, 'Sophia Patel', '1989-09-20', '416-024-8872', '8743210956328901', 'SophiaPatel@example.com', 23, 'Comfort', 11, 0013, 1),
+(0024, 'Ava Khan', '2001-04-22', '467-464-2445', '3456789012345678', 'AvaKhan@example.com', 24, 'Business', 01, 0014, 1);
+
+DROP TABLE IF EXISTS CUSTOMERS;
+CREATE TABLE CUSTOMERS(
+    CUSTOMER_ID         INT,
+    PRIMARY KEY(CUSTOMER_ID),
+    FOREIGN KEY(CUSTOMER_ID) REFERENCES USERS(ID_NO)
+);
+
+INSERT INTO CUSTOMERS(CUSTOMER_ID)
+VALUES
+(0001), (0003), (0004), (0005), (0006), (0007), (0008), (0009), (0010), (0011),
+(0012), (0013), (0014), (0015), (0016), (0017), (0018), (0019), (0020), (0021),
+(0022), (0023), (0024);
+
+DROP TABLE IF EXISTS SEAT; /* Number of seats: 80*/
+/*Seat Numbers - Business: 1 - 10, Comfort: 11 - 25, Ordinary: 26 - 80*/
+CREATE TABLE SEAT(
+    SEAT_TYPE           VARCHAR(40),
+    COST                INT,
+    PRIMARY KEY(SEAT_TYPE)
+);
+
+INSERT INTO SEAT(SEAT_TYPE, COST)
+VALUES
+('Business', 1000),
+('Comfort', 700),
+('Ordinary', 500);
